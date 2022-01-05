9f9fc8882e421c912119d04f6dd129104cdad084
tengyunlong002 <tengyunlong002@ke.com>
2022-01-05 22:20:09 +0800

    add select demo

--- Class26/select_test.go
+++ Class26/select_test.go
@@ -1,7 +1,36 @@
 package class26
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
-func TestSync(t *testing.T) {
+func service() string {
+	time.Sleep(time.Millisecond * 500)
+	return "Done"
+}
 
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 1000)
+	fmt.Println("Task is done.")
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string)
+	go func() {
+		fmt.Println("returned result.")
+		retCh <- service()
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+func TestAsync(t *testing.T) {
+	select {
+	case ret := <-AsyncService():
+		t.Log(ret)
+	case <-time.After(time.Microsecond * 100):
+		t.Error("time out")
+	}
 }
